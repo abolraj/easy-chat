2835ff8207960de61c0fae77dc97be539f95ecda
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-20 13:29:16 +0330

    feat(pages): Add and handle authentication pages

---
+++ client/src/pages/Auth/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../components/auth/RegisterForm";
+
+export default function registerPage() {
+
+    return (
+        <RegisterForm>
+            
+        </RegisterForm>
+    )
+}
