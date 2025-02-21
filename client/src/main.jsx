f7c6e6b65d6855aa0652e3005f8a6ae58e28766c
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-21 14:51:44 +0330

    fix(main): Remove ChatProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
-import { ChatProvider } from './contexts/ChatContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <ChatProvider>
         <App />
-      </ChatProvider>
     </AuthProvider>
   </React.StrictMode>
 )
