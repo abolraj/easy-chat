b6e7a51a902a62ecb3573d8dd9a2feb136d50cda
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-21 21:58:31 +0330

    feat(routes/channels): Handle broadcasting channels

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Models\Conversation;
+use App\Models\Participant;
 use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat.conversation.{conversation_id}', function ($user) {
+    return true;
+    // $participant = Participant::where('conversation_id', $conversation_id)
+    //     ->where('user_id', $user->id)
+    //     ->first();
+    // return !!$participant;
 });
