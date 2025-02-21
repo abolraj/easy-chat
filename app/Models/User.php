5c6804bb5f2adaec5fa6ded5c89ce86a27453b52
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-21 21:54:19 +0330

    feat(models/User): Handle new relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -45,5 +47,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(Participant::class);
+    }
+
+    public function conversations(): BelongsToMany
+    {
+        return $this->belongsToMany( Conversation::class, 'participants', 'user_id', 'conversation_id');
     }
 }
