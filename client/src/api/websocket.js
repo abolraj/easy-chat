848fdc843e6a25bc28fec4127512f7d11c88b4c7
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-21 14:52:23 +0330

    fix(api/websocket): Fix authentication for websocket

--- client/src/api/websocket.js
+++ client/src/api/websocket.js
@@ -2,6 +2,8 @@ import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
 window.Pusher = Pusher
 
+const apiKey = localStorage.getItem('auth_type') + ' ' + localStorage.getItem('auth_token')
+
 const echo = new Echo({
   broadcaster: 'reverb',
   key: 'dvhvfvv6flktjqkasiqx',
@@ -10,6 +12,12 @@ const echo = new Echo({
   wssPort: 9000,
   forceTLS: false,
   withCredentials: true,
+  authEndpoint: 'http://easy-change.localhost:8080/broadcasting/auth',
+  auth:{
+    headers: {
+      Authorization: apiKey,
+    }
+  },
   enabledTransports: ['ws', 'wss'],
 })
 
