1e4b542f49d947f7ebd6a7326a2239028d8dd52f
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-22 21:25:19 +0330

    feat(controllers/ConversationController): Add and handle ConversationController

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -2,26 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ConversationRead;
 use App\Models\Conversation;
 use App\Http\Requests\StoreConversationRequest;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
     public function index()
     {
         return auth()->user()->conversations()
-            ->with(['participants', 'lastMessage'])
-            ->paginate();
+            ->with(['participants', 'messages'])
+            ->latest()
+            ->get();
+    }
+
+    public function show(Request $request, $conversation)
+    {
+        $conversation = Conversation::where('id', $conversation)
+            ->with(['messages', 'messages.user'])
+            ->first();
+
+        $read_participant = $conversation
+            ->participants()
+            ->where('user_id', $request->user()->id)
+            ->first();
+            
+        $read_participant->update(['last_read_at' => (new Carbon(now()))->toDateTimeString()]);
+        info('readeed participant:', $read_participant->toArray());
+        broadcast(new ConversationRead($read_participant));
+
+        return response()->json($conversation);
     }
 
     public function store(StoreConversationRequest $request)
     {
         $conversation = Conversation::create([
             'type' => $request->type,
-            'name' => $request->name
+            'name' => $request->name,
         ]);
 
-        $participants = array_merge([auth()->id()], $request->participants);
+        $participants = array_merge([auth()->id()], $request->users);
         $conversation->participants()->createMany(
             collect($participants)->map(fn($id) => ['user_id' => $id])
         );
