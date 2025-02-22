6778e3ba8ee0357f309a085e428ced2b90afcfea
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-22 21:26:44 +0330

    fix(models/Conversation): Handle fillable properties

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['name', 'type'];
     protected $casts = ['type' => 'string'];
 
     public function participants()
@@ -15,6 +19,6 @@ class Conversation extends Model
 
     public function messages()
     {
-        return $this->hasMany(Message::class)->latest();
+        return $this->hasMany(Message::class)->oldest();
     }
 }
