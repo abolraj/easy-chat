417abc844ad204d9abb934b6bc067935231b386c
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-23 23:24:38 +0330

    fix(env): Handle env vars

--- client/src/api/websocket.js
+++ client/src/api/websocket.js
@@ -3,16 +3,16 @@ import Pusher from 'pusher-js'
 window.Pusher = Pusher
 
 const apiKey = localStorage.getItem('auth_type') + ' ' + localStorage.getItem('auth_token')
-
+console.log(import.meta.env.VITE_ECHO_HTTPS)
 const echo = new Echo({
-  broadcaster: 'reverb',
-  key: 'dvhvfvv6flktjqkasiqx',
-  wsHost: '127.0.0.1',
-  wsPort: 9000,
-  wssPort: 9000,
-  forceTLS: false,
+  broadcaster: import.meta.env.VITE_ECHO_BROADCASTER,
+  key: import.meta.env.VITE_ECHO_KEY,
+  wsHost: import.meta.env.VITE_ECHO_HOST,
+  wsPort: import.meta.env.VITE_ECHO_PORT,
+  wssPort: import.meta.env.VITE_ECHO_PORT,
+  forceTLS: !(import.meta.env.VITE_ECHO_SSL=='http'),
   withCredentials: true,
-  authEndpoint: 'http://easy-change.localhost:8080/broadcasting/auth',
+  authEndpoint: import.meta.env.VITE_LARAVEL_BASE_URL + import.meta.env.VITE_LARAVEL_BROADCAST_AUTH_URL,
   auth:{
     headers: {
       Authorization: apiKey,
