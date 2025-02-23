e52a117d48bc2b5b62d3be394f53c0469cfb4eff
abolraj <abolfazlrajaeenasab@gmail.com>
2025-02-23 23:30:00 +0330

    feat(components/SearchableSelect): Handle multiple options

--- client/src/components/ui/SearchableSelect.jsx
+++ client/src/components/ui/SearchableSelect.jsx
@@ -1,19 +1,34 @@
 import { CheckIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 
-export default function SearchableSelect({ onSearch, onChange, options, inputName, loading, getShowInfo, className = '' }) {
+export default function SearchableSelect({ isMultiple, onSearch, onChange, options, inputName, loading, getShowInfo, className = '', disabled = false }) {
   const [searchTerm, setSearchTerm] = useState(''); // User's search input
-  const [selectedOption, setSelectedOption] = useState(''); // Currently selected option
+  const [selectedOptions, setSelectedOptions] = useState([]); // Currently selected option
 
   // Handle selecting an option
   const handleOptionClick = (option, e) => {
     e.preventDefault()
     console.log('option checked:', option)
     const optionValue = option
-    setSelectedOption(optionValue);
+    let newOptions = optionValue
+    if (isMultiple) {
+      const duplicateIndex = selectedOptions.findIndex(op => op.id == optionValue.id)
+      if (duplicateIndex == -1) {
+        newOptions = [...selectedOptions, optionValue]
+        setSelectedOptions(newOptions)
+      } else {
+        newOptions = [...selectedOptions.slice(0, duplicateIndex), ...selectedOptions.slice(duplicateIndex + 1)]
+        setSelectedOptions(newOptions)
+      }
+    } else {
+      if (selectedOptions.length == 1)
+        setSelectedOptions([]);
+      else
+        setSelectedOptions([optionValue]);
+    }
     setSearchTerm(''); // Reset search term after selection
 
-    onChange(optionValue)
+    onChange(newOptions)
   };
 
   const onSearching = (e) => {
@@ -22,18 +37,27 @@ export default function SearchableSelect({ onSearch, onChange, options, inputNam
   }
 
   return (
-    <div className={"dropdown " + className}>
+    <div className={"dropdown " + className} disabled={disabled}>
       {/* Selected Option Display */}
       <div
         tabIndex={0}
         role="button"
-        className="btn btn-ghost m-1 w-full"
+        className="btn btn-ghost m-1 indicator w-full"
+        disabled={disabled}
       >
-        {getShowInfo(selectedOption).title || 'Select an option'}
+        {selectedOptions?.length && (selectedOptions.length > 1 ?
+          (
+            <div className="w-full">
+              {getShowInfo(selectedOptions[0]).title}
+              <span class="indicator-item indicator-middle indicator-end badge badge-info">+{selectedOptions.length-1}</span>
+            </div>
+          )
+          : getShowInfo(selectedOptions[0]).title
+        ) || 'Select an option'}
       </div>
 
       {/* Dropdown Menu */}
-      <div className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <div className="dropdown-content menu bg-base-100 rounded-box z-1 w-full min-w-52 p-2 shadow-sm">
         {/* Search Input */}
         <label class="input">
           {loading ?
@@ -58,12 +82,12 @@ export default function SearchableSelect({ onSearch, onChange, options, inputNam
                 key={index}
               >
                 <button
-                  // type="button"
-                  onClick={(event) => handleOptionClick(option,event)}
+                  type="button"
+                  onClick={(event) => handleOptionClick(option, event)}
                   className="p-2 hover:bg-base-200 cursor-pointer"
                 >
                   {getShowInfo(option).title}
-                  {selectedOption.id == option.id && <CheckIcon className="h-full text-accent"/>}
+                  {selectedOptions.some(op => op.id == option.id) && <CheckIcon className="h-full text-accent" />}
                 </button>
               </li>
             ))
@@ -73,7 +97,9 @@ export default function SearchableSelect({ onSearch, onChange, options, inputNam
         </ul>
       </div>
 
-      <input type="hidden" name={inputName} value={getShowInfo(selectedOption).value} />
+      {selectedOptions?.map(selectedOption => (
+        <input type="hidden" key={"selected" + selectedOption.id} name={inputName + '[]'} value={getShowInfo(selectedOption).value} />
+      ))}
     </div>
   );
 }
